a4f022ce7976b3036b410bf71c673cece829d3cb
Maxim Veksler <maxim@vekslers.org>
2016-02-22 00:16:45 +0200

    Initiali puse, framework based on post by https://robots.thoughtbot.com/creating-your-first-ios-framework

---
+++ IKnowKungFu/IKnowKungFu.h
@@ -0,0 +1,19 @@
+//
+//  IKnowKungFu.h
+//  IKnowKungFu
+//
+//  Created by Maxim Veksler on 21/02/2016.
+//  Copyright © 2016 Maxim Veksler. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for IKnowKungFu.
+FOUNDATION_EXPORT double IKnowKungFuVersionNumber;
+
+//! Project version string for IKnowKungFu.
+FOUNDATION_EXPORT const unsigned char IKnowKungFuVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IKnowKungFu/PublicHeader.h>
+
+
